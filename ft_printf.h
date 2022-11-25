566fda0726749ad45e0a5a5dc6e902d0199a8f7d
RhesusP <camil.bernot.cb@gmail.com>
2022-11-25 09:26:07 +0100

    synchro for 42

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 20:17:30 by cbernot           #+#    #+#             */
+/*   Updated: 2022/11/24 20:25:15 by cbernot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int ft_printf(const char *, ...);
+
+#endif
