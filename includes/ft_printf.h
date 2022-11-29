ff78148221f8093daad4c7b5c08b279df729c223
Camille Bernot <cbernot@z3r4p1.42lyon.fr>
2022-11-29 16:16:31 +0100

    add comments in header file

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 20:17:30 by cbernot           #+#    #+#             */
-/*   Updated: 2022/11/29 16:12:53 by cbernot          ###   ########.fr       */
+/*   Updated: 2022/11/29 16:16:11 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 
 int		ft_printf(const char *str, ...);
 
+/* print functions */
 int		ft_print_char(unsigned char c);
 int		ft_print_str(char *s);
 int		ft_print_int(int nb);
@@ -26,6 +27,7 @@ int		ft_print_int_base(long nbr, char *base);
 int		ft_print_ptr(unsigned long long nbr);
 int		ft_print_uint(unsigned int nb);
 
+/* utils */
 size_t	ft_strlen(const char *s);
 
 #endif
